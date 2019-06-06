df71ab38b04bfb58b8e87d2c30b8691e46f8df4e
Tuan Vuong <vanhtuan0409@gmail.com>
2019-06-06 09:43:57 +0700

    add notes

--- http.go
+++ http.go
@@ -10,6 +10,8 @@ func handleHTTPProxy(rw http.ResponseWriter, r *http.Request) {
 	log.Printf("Proxy for request: [HTTP] %s - %s - %s", r.Method, r.Host, r.RequestURI)
 	// Can optimize using customize Transporter
 	// to adjust number of keep-alive and reuse connection
+	// Don't use HTTP Client because it will follow all redirection
+	// We better let the client handle the redirect message
 	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
 		log.Printf("Cannot execute request. ERR: %v\n", err)
